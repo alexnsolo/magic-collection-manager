af80c28bed003b2a3c40756133ea5cee65f62ea7
forana <alexdforan@gmail.com>
2014-04-15 22:46:28 -0500

    phyrexian oops

--- backend/serve.js
+++ backend/serve.js
@@ -63,7 +63,7 @@ app.get("/image/:id", function(req, res) {
 
 app.get("/icon/:id", function(req, res) {
     var id = req.param("id");
-    if (/^P?[RGBUW]$/.test(id) ||
+    if (/^[RGBUW]P?$/.test(id) ||
             /^2[RGBUW]$/.test(id) ||
             /^[0-9]+$/.test(id) ||
             id == "SNOW") {
